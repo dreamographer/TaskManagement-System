5de9831926bbfc95b11d9fcef8e8b4ed19e78686
Ashwin <ashwinkv.akv@gmail.com>
2024-07-25 16:21:55 +0530

    initial Project setup with Mongodb setup set up the nest.js project structure with 2 services added mongodb setup

---
+++ backend/task-managemet-app/apps/task-management/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { TaskManagementModule } from './task-management.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(TaskManagementModule);
+  await app.listen(3000);
+}
+bootstrap();
